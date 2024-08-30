4af80e8d0d6f3dd27f68fb086ce9116dfd6af161
Rebecca Le <traybaby@gmail.com>
2024-08-31 01:35:16 +0800

    Add Kickstarter example

--- lib/phoenix_tailwind_progress_bar_example_web/live/demo_live.ex
+++ lib/phoenix_tailwind_progress_bar_example_web/live/demo_live.ex
@@ -9,25 +9,38 @@ defmodule PhoenixTailwindProgressBarExampleWeb.DemoLive do
     ~H"""
     <div id="demo-live">
       <div :for={value <- 10..100//10} class="mt-4 h-4 w-full">
-        <progress
-          value={value}
-          max="100"
-          class={
-            [
-              "h-full w-full align-top appearance-none rounded-full hover:scale-y-150",
-              # Filled styles
-              "filled:rounded-full filled:animate-pulse filled:bg-gradient-to-r",
-              "filled:from-indigo-500 filled:via-purple-500 filled:to-pink-500",
-              # Filled hover styles
-              "filled:hover:animate-none filled:hover:from-cyan-500",
-              "filled:hover:via-sky-500 filled:hover:to-green-500",
-              # Unfilled styles
-              "unfilled:bg-slate-100"
-            ]
-          }
-        />
+        <div class="h-full w-full bg-slate-100 rounded-full">
+          <progress
+            value={value}
+            max="100"
+            class={[
+              "h-full w-full align-top appearance-none rounded-full",
+              "filled:rounded-full unfilled:bg-slate-100 animate-[size_2s_ease-out]",
+              temperature(value)
+            ]}
+          />
+        </div>
       </div>
     </div>
     """
+  end
+
+  def temperature(val) do
+    case val do
+      val when val <= 20 ->
+        "filled:bg-red-500"
+
+      val when val in 21..40 ->
+        "filled:animate-[orange_2s_ease-out] filled:bg-orange-500"
+
+      val when val in 41..60 ->
+        "filled:animate-[yellow_2s_ease-out] filled:bg-yellow-500"
+
+      val when val in 61..80 ->
+        "filled:animate-[lime_2s_ease-out] filled:bg-lime-500"
+
+      _ ->
+        "filled:animate-[green_2s_ease-out] filled:bg-green-500"
+    end
   end
 end
